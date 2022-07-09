56980b899bb7c5a274b8df0a2ea06dc33355fcf2
willyrol <85806108+willyrol@users.noreply.github.com>
2022-07-09 15:07:09 +1200

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-function getPassword() {
-  const chars =
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%";
+  const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+  
+  const passOne = document.getElementById("one");
+  const passTwo = document.getElementById("two");
+  const button = document.getElementById("button");
+  let passwordLength = 15;
+  
 
-  let passwordLength = 16;
-  let password = "";
-
-  for (let i = 0; i < passwordLength; i++) {
-    let randomNumber = Math.floor(Math.random() * chars.length);
-
-    password += chars.substring(randomNumber, randomNumber + 1);
-  }
+function firstPass() {
+    let randomPassOne = Math.floor(Math.random() * characters.length);
+    return characters[randomPassOne]
 }
 
+function secondPass() {
+    let randomPassTwo = Math.floor(Math.random() * characters.length);
+    return characters[randomPassTwo]
+}
 
-  document.getElementById("one").textContent = password;
-  document.getElementById("two").textContent = password;
-  document.getElementById("three").textContent = password;
-  document.getElementById("four").textContent = password;
+function generatePassword() {
+    let randomPasswordOne = ""
+    let randomPasswordTwo = ""
+    
+    for ( let i = 0; i < passwordLength; i++) {
+        randomPasswordOne += firstPass()
+    }
+    for ( let i = 0; i < passwordLength; i++) {
+        randomPasswordTwo += secondPass()
+    }
+    passOne.textContent = randomPasswordOne
+    passTwo.textContent = randomPasswordTwo
+}
+function resetPassword() {
+    passOne.textContent = ""
+    passTwo.textContent = ""
+}
